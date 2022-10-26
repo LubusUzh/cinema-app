4b5ae590d6844a98bad934b120a2183d841805d3
LubusUzh <lubus.uzh@gmail.com>
2022-10-26 14:18:05 +0300

    Add: files to the project cinema-app

---
+++ src/main/java/cinema/DataInitializer.java
@@ -0,0 +1,32 @@
+package cinema;
+
+import cinema.model.Role;
+import cinema.model.User;
+import cinema.service.RoleService;
+import cinema.service.UserService;
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class DataInitializer {
+    private final RoleService roleService;
+    private final UserService userService;
+
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setRoleName(Role.RoleName.ADMIN);
+        roleService.add(adminRole);
+        Role userRole = new Role();
+        userRole.setRoleName(Role.RoleName.USER);
+        roleService.add(userRole);
+        User user = new User();
+        user.setEmail("admin@i.ua");
+        user.setPassword("admin123");
+        user.setRoles(Set.of(adminRole));
+        userService.add(user);
+    }
+}
